a430661d023afe74ad1315688f7bcb2ee83e290d
Jennifer Cafiero <jcafiero@stevens.edu>
2018-11-16 01:39:15 -0500

    Adds Title, Navbar, and Dropdown to Make Page. Adjusts footer so it no longer overlaps over long form. Adds aria-labels to every input for accessibility

---
+++ home.js
@@ -0,0 +1,155 @@
+$("#card").flip();
+var title = [];
+var front = [];
+var back = [];
+count = 0;
+formCount = 0;
+var deckName = document.getElementById("deck-name");
+
+function init() {
+	$("#current").append(front[count]);
+	$(".back").append(back[count]);
+	$("#deck-name").append(title[count]);
+	if(deckName) {
+        $(".home-title").css("display", "none");
+        $(".navbar").css("display", "none");
+        $(".select-deck").css("display", "none");
+	}
+}
+
+<!--Movement Functions-->
+function forward() {
+	$("#current").empty()
+	$(".back").empty();
+	if(count >= front.length-1) {
+		count = 0;
+	} else {
+		count++;
+	}
+	$("#current").append(front[count]);
+	$(".back").append(back[count]);
+	$("#card").flip(false);
+	$("#deck").effect("shake", {distance:5}, "slow");
+	borderMove();
+}
+
+function prev() {
+	$("#current").empty()
+	$(".back").empty();
+	if(count <= 0) {
+		count = front.length-1;
+	} else {
+		count--;
+	}
+	$("#current").append(front[count]);
+	$(".back").append(back[count]);
+	$("#card").flip(false);
+}
+
+function restart() {
+	$("#current").empty()
+	$(".back").empty();
+	count = 0;
+	$("#current").append(front[count]);
+	$(".back").append(back[count]);
+	$("#card").flip(false);
+}
+
+function shuffle() {
+
+	// Both arrays should be the same length
+	let counter = front.length;
+
+	// Iterate over whole array
+	while (counter > 0) {
+
+		// Random index
+		let index = Math.floor(Math.random() * counter);
+
+		counter--;
+
+		// Shuffle front
+		let temp1 = front[counter];
+		front[counter] = front[index];
+		front[index] = temp1;
+
+		// Shuffle back
+		let temp2 = back[counter];
+		back[counter] = back[index];
+		back[index] = temp2;
+
+	}
+}
+
+<!--Other funcs-->
+function addForm() {
+	formCount++;
+	$(".deck-form").append(`<div>
+		<textarea id="front${formCount}" name="front${formCount}" placeholder="Question" class="cardEntry" {
+			constructor() {
+
+			}
+		}></textarea>
+		<textarea id="back${formCount}" name="back${formCount}" placeholder="Answer" class="cardEntry"></textarea>
+		</div>`);
+	}
+
+	function sendTo() {
+		$("form :input").each(function(index) {
+			if(index == 0){
+				title.push($(this).val());
+			}else if(index % 2 != 0) {
+				front.push($(this).val());
+			} else {
+				back.push($(this).val());
+			}
+		});
+		$(".form").css("display", "none");
+		$(".cards").css("display", "unset");
+		init();
+	}
+	function borderMove() {
+		$(".front-deck").transfer({
+			to: $("#current"),
+			className: "border-transfer",
+			duration: 500
+		}, function(x) {
+			$("#current").css("border", "3px solid orange");
+		});
+	}
+
+
+	<!--Movement-->
+	$("#card").on("click", function(x) {
+		if($("#current").css("border")) {
+			$("#current").css("border", "");;
+		}
+	});
+
+	$("#deck").on("click", function(x) {
+		forward();
+	});
+
+	$("#next").on("click", function(x) {
+		forward();
+	});
+
+	$("#prev").on("click", function(x) {
+		prev();
+	});
+
+	$("#restart").on("click", function(x) {
+		restart();
+	});
+
+	$("#shuffle").on("click", function(x) {
+		shuffle();
+	});
+
+	$(".add").on("click", function(x) {
+		addForm();
+	});
+
+	$(".submit").on("click", function(x) {
+		sendTo();
+	});
